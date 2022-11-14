08a8de3f6c01887ff404f4e924b551367be13738
farhinpatel <118152737+farhinpatel@users.noreply.github.com>
2022-11-14 10:54:02 +0530

    Add files via upload

---
+++ Sequence/DistanceConversion.java
@@ -0,0 +1,30 @@
+package Sequence;
+
+import java.util.Scanner;
+
+public class DistanceConversion {
+
+	public static void main(String[] args) {
+		
+		double km,m,cm,mm;
+		
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the distance in kilometers");
+        
+        km=input.nextDouble();
+        
+        m=1000*km;
+        cm=100*m;
+        mm=10*cm;
+        
+        System.out.println("Distance in kilometers ="+km);
+        System.out.println("Distance in meters ="+m);
+        System.out.println("Distance in centimeters ="+cm);
+        System.out.println("Distance in milimeters ="+mm);
+        
+        
+
+	}
+
+}
