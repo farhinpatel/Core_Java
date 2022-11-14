493aebf4c5a01f8e6e19f1cf7492a5f6f2ff4528
farhinpatel <118152737+farhinpatel@users.noreply.github.com>
2022-11-14 10:28:13 +0530

    Add files via upload

---
+++ Operaters/RelationalOperators.java
@@ -0,0 +1,21 @@
+package Operators;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// <,>,<=,>=,!=,==
+		
+		System.out.println(6<9);
+		System.out.println(3>5);
+		
+		int a=45,b=80;
+		
+		System.out.println(a<b);
+		
+		System.out.println(a!=b);
+		
+		System.out.println(a==b);
+
+	}
+
+}
