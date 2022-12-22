0c6f17dca2d22fc6a9394105b2100ef4750a2913
farhinpatel <118152737+farhinpatel@users.noreply.github.com>
2022-12-22 09:49:03 +0530

    Add files via upload

---
+++ accessexample/TestData.java
@@ -0,0 +1,17 @@
+package accessex;
+
+import testpack2.Data;
+
+public class TestData {
+
+	public static void main(String[] args) {
+                                    
+		Data d2 = new Data();
+		
+		testpack.Data d1 = new testpack.Data();
+		 
+		d2.m1();
+		d1.m1();
+	}
+
+}
