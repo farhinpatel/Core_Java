c8436262b774a02fdc1ff1f9b8f032cc5b053418
farhinpatel <118152737+farhinpatel@users.noreply.github.com>
2022-12-23 10:02:17 +0530

    Add files via upload

---
+++ bitandPieces/LocalVariable1.java
@@ -0,0 +1,14 @@
+package bitandPieces;
+
+public class LocalVariable1 {
+
+	public static void main(String[] args) {
+ 
+		for(int x=1;x<=5;x++)
+			System.out.println(x);
+		
+		
+		//System.out.println("x = "x);
+	}
+
+}
